31ea677be72f17668368bac36a5e384ac07597cc
Lars Kecker <lars.kecker2009@gmail.com>
2021-12-18 00:26:55 +0100

    temperature_converter: loop and cleanup

--- temperature_converter/src/main.rs
+++ temperature_converter/src/main.rs
@@ -1,36 +1,44 @@
 use std::io;
 
 fn main() {
+    loop {
+        println!("Enter 1 to convert Fahrenheit to Celsius, 2 to convert Celsius to Fahrenheit or anything else to exit the program.");
+        let mut operation = String::new();
+        io::stdin()
+            .read_line(&mut operation)
+            .expect("Error while trying to read line.");
 
-    println!("Enter 1 to convert Fahrenheit to Celsius, 2 to convert Celsius to Fahrenheit or 0 to exit the program.");
-    let mut operation = String::new();
+        match operation.trim().parse() {
+            Ok(1) => {
+                let unconverted_value = get_unconverted_value("Fahrenheit");
+                let result = convert_to_celsius(unconverted_value);
+                println!("{} °F is equal to {} °C.", unconverted_value, result);
+            }
+            Ok(2) => {
+                let unconverted_value = get_unconverted_value("Celsius");
+                let result = convert_to_fahrenheit(unconverted_value);
+                println!("{} °C is equal to {} °F.", unconverted_value, result);
+            }
+            Ok(_) => break,
+            Err(_) => break,
+        };
+    }
+    println!("Goodbye.");
+}
 
-    io::stdin()
-        .read_line(&mut operation)
-        .expect("Error while trying to read line.");
-    
-    let operation: u8 = operation.trim().parse().expect("Please type a number");
-
+fn get_unconverted_value(expected_type: &str) -> f64 {
     let mut unconverted_value = String::new();
-    
-    println!("Enter the temperature value to convert:");
+
+    println!("Enter the temperature in {} to convert:", expected_type);
 
     io::stdin()
         .read_line(&mut unconverted_value)
         .expect("Error while trying to read line.");
 
-    let unconverted_value: f64 = unconverted_value.trim().parse().expect("Please enter a valid decimal number.");
-
-    let result = if operation == 1 {
-        convert_to_celsius(unconverted_value)
-    } else if operation == 2 {
-        convert_to_fahrenheit(unconverted_value)
-    } else {
-        0.0
-    };
-
-    println!("Your result is {}", result);
-
+    unconverted_value
+        .trim()
+        .parse()
+        .expect("Please enter a valid decimal number.")
 }
 
 fn convert_to_fahrenheit(temperature_celsius: f64) -> f64 {
