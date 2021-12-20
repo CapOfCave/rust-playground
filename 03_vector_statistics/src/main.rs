cdf036f1fba7f5adefc9b4dd0d0659b400a26836
Lars Kecker <lars.kecker2009@gmail.com>
2021-12-20 21:03:06 +0100

    vector_statistics: add mode

--- 03_vector_statistics/src/main.rs
+++ 03_vector_statistics/src/main.rs
@@ -1,12 +1,13 @@
-use std::collections::HashMap{self, Entry};
+use std::collections::HashMap;
 
 fn main() {
     println!("Hello, world!");
 
-    let mylist = vec![1, 2, 8, 4, 6, 9];
+    let mylist = vec![1, 2, 6, 4, 6, 11];
 
     println!("The mean is {}.", mean(&mylist));
     println!("The median is {}.", median(&mylist));
+    println!("The mode is {}.", mode(&mylist));
 }
 
 fn mean(input: &Vec<i32>) -> i32 {
@@ -24,4 +25,21 @@ fn median(input: &Vec<i32>) -> i32 {
     let lower = sorted[(sorted.len() - 1) / 2];
     let upper = sorted[sorted.len() / 2];
     (lower + upper) / 2
+}
+
+fn mode(input: &Vec<i32>) -> i32 {
+    let mut occurences: HashMap<i32, i32> = HashMap::with_capacity(input.len());
+    for i in input {
+        let count = occurences.entry(*i).or_insert(0);
+        *count += 1;
+    }
+    let mut max_key = -1;
+    let mut max_value = i32::MIN;
+    for (key, value) in occurences {
+        if value > max_value {
+            max_key = key;
+            max_value = value;
+        }
+    }
+    max_key
 }
