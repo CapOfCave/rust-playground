64a2771fe1959bbaba7e711ae2e36e9506431b7e
Lars Kecker <lars.kecker2009@gmail.com>
2021-12-21 12:02:43 +0100

    piglatin: optimizations

--- 04_piglatin/src/main.rs
+++ 04_piglatin/src/main.rs
@@ -1,39 +1,39 @@
 fn main() {
-    println!("{}", to_pig_latin("this is a really cool string"));
+    println!("{}", to_pig_latin("this is a  really cool string "));
 }
 
 const VOWELS: &str = "aeiou";
 
 fn to_pig_latin(source: &str) -> String {
-    let mut word = String::new();
-    let mut out = String::new();
-    for character in source.chars() {
+    let mut out = String::with_capacity(source.len()); // will have at least the size of source
+    let mut word_start = 0;
+    let mut word_end = 0;
+    for (index, character) in source.char_indices() {
         if character.is_whitespace() {
-            out.push_str(&to_pig_latin_word(&word));
+            out.push_str(&to_pig_latin_word(&source[word_start..word_end]));
             out.push(' ');
-            word.clear();
+            word_start = index + 1;
+            word_end = word_start;
         } else {
-            word.push(character);
+            word_end += 1;
         }
     }
-    out.push_str(&word);
+    out.push_str(&to_pig_latin_word(&source[word_start..word_end]));
     out
 }
 
 fn to_pig_latin_word(word: &str) -> String {
-    let mut first: Option<char> = None;
     let mut result = String::with_capacity(word.len() + 4);
-    for (index, character) in word.char_indices() {
-        if index == 0 {
-            if VOWELS.contains(character) {
-                first = Some('h');
-                result.push(character);
-            } else {
-                first = Some(character)
-            }
-        } else {
+    let first = word.chars().next().map(|character| {
+        if VOWELS.contains(character) {
             result.push(character);
+            'h'
+        } else {
+            character
         }
+    });
+    for character in word.chars().skip(1) {
+        result.push(character);
     }
     if let Some(character) = first {
         result.push('-');
